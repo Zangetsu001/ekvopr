3785666be3ceac70fd36e5693e648d5c3efdc935
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-04-22 22:42:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address myAddress = new Address();
+
+            myAddress.Index = "5373223";
+            myAddress.Country = "ПМР";
+            myAddress.City = "Тирасполь";
+            myAddress.Street = "Магистральная";
+            myAddress.House = "12";
+            myAddress.Apartment = "45";
+
+            Console.WriteLine("Почтовый адрес:");
+            Console.WriteLine($"Индекс: {myAddress.Index}");
+            Console.WriteLine($"Страна: {myAddress.Country}");
+            Console.WriteLine($"Город: {myAddress.City}");
+            Console.WriteLine($"Улица: {myAddress.Street}");
+            Console.WriteLine($"Дом: {myAddress.House}");
+            Console.WriteLine($"Квартира: {myAddress.Apartment}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
