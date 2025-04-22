047e01916a3f3e8807f1ed01fd9cee5ca9f51dba
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-04-22 22:32:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину первой стороны прямоугольника: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите длину второй стороны прямоугольника: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rect = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Площадь прямоугольника: {rect.Area}");
+            Console.WriteLine($"Периметр прямоугольника: {rect.Perimeter}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
