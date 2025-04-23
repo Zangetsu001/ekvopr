edf69f553f0bb43190214b3b1151e01f885e063b
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-04-23 20:30:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите логин:");
+            string login = Console.ReadLine();
+
+            Console.WriteLine("Введите имя:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            User user = new User(login, firstName, lastName, age);
+            user.ShowInfo();
+
+      
+            Console.ReadKey();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+  
+        public readonly DateTime RegistrationDate;
+
+        public User(string login, string firstName, string lastName, int age)
+        {
+            Login = login;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            RegistrationDate = DateTime.Now;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("\nИнформация о пользователе:");
+            Console.WriteLine("Логин: " + Login);
+            Console.WriteLine("Имя: " + FirstName);
+            Console.WriteLine("Фамилия: " + LastName);
+            Console.WriteLine("Возраст: " + Age);
+            Console.WriteLine("Дата заполнения анкеты: " + RegistrationDate.ToString("dd.MM.yyyy HH:mm"));
+        }
+    }
+}
