5936cf607a1f316c7a57ca9e8dab7edbfbb2f7b4
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-04-23 20:04:21 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Employee
+    {
+        private string firstName;
+        private string lastName;
+
+        public Employee(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void CalculateSalary(string position, int experience, out double salary, out double tax)
+        {
+            double baseSalary;
+
+   
+            if (position.ToLower() == "менеджер")
+                baseSalary = 800;
+            else if (position.ToLower() == "инженер")
+                baseSalary = 1000;
+            else if (position.ToLower() == "директор")
+                baseSalary = 1500;
+            else
+                baseSalary = 500;
+
+
+            double experienceBonus = baseSalary * 0.03 * experience;
+            salary = baseSalary + experienceBonus;
+
+            tax = salary * 0.13;
+        }
+
+        public string GetFullName()
+        {
+            return lastName + " " + firstName;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя сотрудника:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию сотрудника:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Введите должность (менеджер, инженер, директор и т.д.):");
+            string position = Console.ReadLine();
+
+            Console.WriteLine("Введите стаж (в годах):");
+            int experience = Convert.ToInt32(Console.ReadLine());
+
+            Employee employee = new Employee(firstName, lastName);
+
+            double salary, tax;
+            employee.CalculateSalary(position, experience, out salary, out tax);
+
+            Console.WriteLine("\nИнформация о сотруднике:");
+            Console.WriteLine("ФИО: " + employee.GetFullName());
+            Console.WriteLine("Должность: " + position);
+            Console.WriteLine("Оклад: {0:F2} BYN", salary);
+            Console.WriteLine("Налоговый сбор (13%): {0:F2} BYN", tax);
+
+            Console.ReadKey();
+        }
+    }
+}
