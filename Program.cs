feaf14aeb0c79e28c103021f290e07b593ba6f22
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-05-01 10:54:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ключ (pro / exp), или нажмите Enter для бесплатной версии: ");
+            string key = Console.ReadLine();
+
+            DocumentWorker doc;
+
+            if (key == "exp")
+            {
+                doc = new ExpertDocumentWorker();
+                Console.WriteLine("Экспертная версия активирована.");
+            }
+            else if (key == "pro")
+            {
+                doc = new ProDocumentWorker();
+                Console.WriteLine("Про-версия активирована.");
+            }
+            else
+            {
+                doc = new DocumentWorker();
+                Console.WriteLine("Бесплатная версия активирована.");
+            }
+
+          
+            Console.WriteLine("Работа с документом:");
+            doc.OpenDocument();
+            doc.EditDocument();
+            doc.SaveDocument();
+
+
+            Console.ReadKey();
+        }
+    }
+}
