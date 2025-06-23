4e02373c42e9451b6fb49b15d114f37ea17eca27
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-23 17:27:14 +0300

    Add files via upload

---
+++ Figura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Figura
+    {
+        public void ShowArea(double side)
+        {
+            Console.WriteLine("Фигура: Квадрат");
+            Console.WriteLine("Площадь: " + (side * side));
+        }
+
+       
+        public void ShowArea(double length, double width)
+        {
+            Console.WriteLine("Фигура: Прямоугольник");
+            Console.WriteLine("Площадь: " + (length * width));
+        }
+
+        
+        public void ShowArea(double base1, double base2, double height)
+        {
+            Console.WriteLine("Фигура: Трапеция");
+            Console.WriteLine("Площадь: " + ((base1 + base2) * height / 2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura f = new Figura();
+
+            Console.WriteLine("Выберите фигуру:");
+            Console.WriteLine("1 - Квадрат\n2 - Прямоугольник\n3 - Трапеция");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Введите сторону квадрата: ");
+                    double side = double.Parse(Console.ReadLine());
+                    f.ShowArea(side);
+                    break;
+
+                case "2":
+                    Console.Write("Введите длину: ");
+                    double length = double.Parse(Console.ReadLine());
+                    Console.Write("Введите ширину: ");
+                    double width = double.Parse(Console.ReadLine());
+                    f.ShowArea(length, width);
+                    break;
+
+                case "3":
+                    Console.Write("Введите основание 1: ");
+                    double base1 = double.Parse(Console.ReadLine());
+                    Console.Write("Введите основание 2: ");
+                    double base2 = double.Parse(Console.ReadLine());
+                    Console.Write("Введите высоту: ");
+                    double height = double.Parse(Console.ReadLine());
+                    f.ShowArea(base1, base2, height);
+                    break;
+
+                default:
+                    Console.WriteLine("Неверный выбор.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
