fbaf6005903a52072f1a3ebb6c9a7ebcf4f5d2ce
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-23 17:13:52 +0300

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class MyClass
+    {
+        public void ShowInterfaceA()
+        {
+            Console.WriteLine("ShowInterfaceA() is completed.");
+        }
+
+        public void ShowInterfaceB()
+        {
+            Console.WriteLine("ShowInterfaceB() is completed.");
+        }
+
+        public void ShowInterfaceC()
+        {
+            Console.WriteLine("ShowInterfaceC() is completed.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Exploring Interfaces.Example-5***");
+            Console.WriteLine("***Concept of multiple inheritance through interface***\n");
+
+            MyClass myClassOb = new MyClass();
+            myClassOb.ShowInterfaceA();
+            myClassOb.ShowInterfaceB();
+            myClassOb.ShowInterfaceC();
+
+            Console.ReadLine();
+        }
+    }
+}
