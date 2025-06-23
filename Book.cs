b16776c5fbb2dcad8e4e1bba739a916751258955
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-23 17:38:01 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    struct Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+        public int Pages;
+
+        // Конструктор
+        public Book(string title, string author, int year, int pages)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Pages = pages;
+        }
+
+        // Метод вывода информации
+        public void Display()
+        {
+            Console.WriteLine($"{Author} {Title} {Year}г {Pages}с");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[]
+           {
+                new Book("Программирование на языке высокого уровня", "Павловская И.А.", 2013, 430),
+                new Book("Полный справочник по C# 4.0", "Герберт Шилдт", 2010, 900),
+                new Book("Понятно о Visual Basic.Net. Самоучитель. В трёх томах", "Лукьян С.Н.", 2005, 800),
+                new Book("Практикум по Visual Basic 2007", "Усева О.П.", 2007, 544)
+           };
+
+            // Вывод всех книг
+            foreach (Book book in books)
+            {
+                book.Display();
+            }
+
+            // Задержка экрана
+            Console.ReadLine();
+        }
+    }
+}
