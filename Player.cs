e56ed4d9891a9524b212f8513f033ed3620b55a1
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-23 17:05:41 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    // Класс, реализующий оба интерфейса
+    class Player : IPlayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing...");
+        }
+
+        public void Record()
+        {
+            Console.WriteLine("Recording...");
+        }
+
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Paused...");
+        }
+
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Paused...");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Stopped...");
+        }
+
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Stopped...");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            // Работа как IPlayable
+            IPlayable playable = player;
+            playable.Play();
+            playable.Pause();
+            playable.Stop();
+
+            Console.WriteLine();
+
+            // Работа как IRecodable
+            IRecodable recodable = player;
+            recodable.Record();
+            recodable.Pause();
+            recodable.Stop();
+
+            Console.ReadLine();
+        }
+    }
+    
+}
