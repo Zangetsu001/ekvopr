bc7359117b5f983768d1f5df70f21cf7da89bce5
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-24 14:25:10 +0300

    Add files via upload

---
+++ Accauntant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    enum Post
+    {
+        Manager = 180,
+        Developer = 170,
+        Analyst = 175,
+        Designer = 160,
+        Tester = 165
+    }
+
+    internal class Accauntant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            return hours > (int)worker;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите должность сотрудника:");
+            string inputPost = Console.ReadLine();
+
+            Console.WriteLine("Введите количество отработанных часов:");
+            string hoursStr = Console.ReadLine();
+            int hours;
+
+            if (!int.TryParse(hoursStr, out hours))
+            {
+                Console.WriteLine("Ошибка: введите корректное количество часов.");
+                return;
+            }
+
+            Post workerPost;
+            bool validPost = false;
+
+            // Перебор вручную для совместимости с C# 7.3
+            foreach (Post post in Enum.GetValues(typeof(Post)))
+            {
+                if (post.ToString().Equals(inputPost, StringComparison.OrdinalIgnoreCase))
+                {
+                    workerPost = post;
+                    validPost = true;
+
+                    Accauntant accountant = new Accauntant();
+                    bool hasBonus = accountant.AskForBonus(workerPost, hours);
+
+                    if (hasBonus)
+                        Console.WriteLine("Сотруднику положена премия.");
+                    else
+                        Console.WriteLine("Сотруднику не положена премия.");
+
+                    break;
+                }
+            }
+
+            if (!validPost)
+            {
+                Console.WriteLine("Ошибка: такой должности не существует.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+   
+}
