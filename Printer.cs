84b7a07b19c6e231e0efeffab9aa8360c4a2dccc
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-24 14:20:38 +0300

    Add files via upload

---
+++ Printer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    enum TextColor
+    {
+        Black = 0,
+        Blue,
+        Green,
+        Cyan,
+        Red,
+        Magenta,
+        Yellow,
+        White
+    }
+    internal class Printer
+    {
+        public static void Print(string stroka, int color)
+        {
+            if (Enum.IsDefined(typeof(TextColor), color))
+            {
+                Console.ForegroundColor = (ConsoleColor)color;
+                Console.WriteLine($"\n{stroka}");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("Неверный номер цвета.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string userText = Console.ReadLine();
+
+            Console.WriteLine("\nДоступные цвета:");
+            foreach (var color in Enum.GetValues(typeof(TextColor)))
+            {
+                Console.WriteLine($"{(int)color}. {color}");
+            }
+
+            Console.Write("\nВведите номер цвета: ");
+            if (int.TryParse(Console.ReadLine(), out int selectedColor))
+            {
+                Printer.Print(userText, selectedColor);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
