d8e781fb4589b41030251f7663d80a65c3f8476b
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:06:39 +0300

    Add files via upload

---
+++ Computer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    internal class Computer
+    {
+        public string Model { get; set; }
+
+        private int ram;
+        public int RAM
+        {
+            get => ram;
+            set => ram = (value >= 2 && value <= 32) ? value : throw new ArgumentException("RAM от 2 до 32");
+        }
+
+        private int hdd;
+        public int HDD
+        {
+            get => hdd;
+            set => hdd = (value >= 200 && value <= 2000) ? value : throw new ArgumentException("HDD от 200 до 2000");
+        }
+
+        public double GetCost() => RAM * 10 + HDD * 0.5;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer[] comps = {
+            new Computer { Model = "Model A", RAM = 8, HDD = 500 },
+            new Computer { Model = "Model B", RAM = 16, HDD = 1000 }
+        };
+
+            double total = 0;
+            foreach (var c in comps)
+            {
+                double cost = c.GetCost();
+                Console.WriteLine($"{c.Model} стоит {cost} руб.");
+                total += cost;
+            }
+
+            Console.WriteLine($"Общая стоимость: {total} руб.");
+        }
+    }
+}
