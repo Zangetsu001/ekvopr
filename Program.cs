0fe4d58de232b05127d0da0570587818c720ed0c
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:09:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var triangle = new Triangle<double>(3, 4, 5);
+            triangle.Print();
+            Console.WriteLine("Площадь: " + triangle.Area());
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp48
+{
+    internal class Triangle<T> where T : struct, IConvertible
+    {
+        private T a, b, c;
+
+        public T A
+        {
+            get => a;
+            set => a = value;
+        }
+
+        public T B
+        {
+            get => b;
+            set => b = value;
+        }
+
+        public T C
+        {
+            get => c;
+            set => c = value;
+        }
+
+        public Triangle(T a, T b, T c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Стороны: A = {a}, B = {b}, C = {c}");
+        }
+
+        public double Area()
+        {
+            double da = Convert.ToDouble(a);
+            double db = Convert.ToDouble(b);
+            double dc = Convert.ToDouble(c);
+            double p = (da + db + dc) / 2;
+            return Math.Sqrt(p * (p - da) * (p - db) * (p - dc));
+        }
+    }
+}
