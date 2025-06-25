1c4e1b18f8f47682203c24c923d1ccc8a53b3034
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 19:51:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tovar[] tovary = {
+            new Tovar { Name = "Молоко", Price = 15, Kvo = 2 },
+            new Tovar { Name = "Хлеб", Price = 5, Kvo = 3 }
+        };
+
+            int total = 0;
+            foreach (var t in tovary)
+            {
+                t.Show();
+                total += t.GetCost();
+            }
+
+            Console.WriteLine($"Итого: {total} руб.");
+        }
+    }
+}
---
+++ Tovar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    internal class Tovar
+    {
+        private string name;
+        private int price;
+        private int kvo;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Price
+        {
+            get => price;
+            set => price = (value >= 1 && value <= 20) ? value : 1;
+        }
+
+        public int Kvo
+        {
+            get => kvo;
+            set => kvo = (value >= 0 && value <= 10) ? value : 0;
+        }
+
+        public int GetCost()
+        {
+            return Price * Kvo;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{Name}: {Kvo} шт по {Price} руб = {GetCost()} руб.");
+        }
+    }
+}
