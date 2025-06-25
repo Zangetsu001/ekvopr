fdf83abc76c9f8228d484620111741d65188b378
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 19:54:02 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    internal class Calculator
+    {
+        public static double Add(double a, double b) => a + b;
+        public static double Sub(double a, double b) => a - b;
+        public static double Mul(double a, double b) => a * b;
+        public static double Div(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException("Деление на ноль!");
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите первое число: ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("Выберите операцию (+, -, *, /): ");
+                string op = Console.ReadLine();
+
+                double result = 0;
+                switch (op)
+                {
+                    case "+": result = Calculator.Add(a, b); break;
+                    case "-": result = Calculator.Sub(a, b); break;
+                    case "*": result = Calculator.Mul(a, b); break;
+                    case "/": result = Calculator.Div(a, b); break;
+                    default: Console.WriteLine("Неизвестная операция."); return;
+                }
+
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
