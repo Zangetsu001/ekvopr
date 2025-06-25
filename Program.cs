c76643b6c46ca99da84dae4b465af149fb5acc14
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 19:50:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tour t = new Tour();
+            t.TourCalc();
+            t.TourCalc("Молдова");
+            t.TourCalc("Румыния", 5);
+        }
+    }
+}
---
+++ Tour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    internal class Tour
+    {
+        public void TourCalc()
+        {
+            Console.WriteLine("Минское море. Стоимость: бесплатно");
+        }
+
+        public void TourCalc(string country)
+        {
+            Console.WriteLine($"{country}, 1 день. Стоимость: 50 руб.");
+        }
+
+        public void TourCalc(string country, int days)
+        {
+            Console.WriteLine($"{country}, {days} дн. Стоимость: {50 * days} руб.");
+        }
+    }
+}
