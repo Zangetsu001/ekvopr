51191c95ab4491e92885815825bc947fbb3d8c14
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:08:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Point<int>(3, 4);
+            var p2 = new Point<uint>(10u, 20u);
+            var p3 = new Point<double>(5.5, 6.6);
+
+            p1.Print();
+            p2.Print();
+            p3.Print();
+        }
+    }
+}
