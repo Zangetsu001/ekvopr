51c8af853a5867fa7b2fd86730b0eb29ad6816af
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:15:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Zakaz>();
+            while (true)
+            {
+                Console.Write("Введите фамилию (или Q для выхода): ");
+                string fam = Console.ReadLine();
+                if (fam.ToUpper() == "Q") break;
+
+                Console.Write("Количество мест: ");
+                int size = int.Parse(Console.ReadLine());
+                Console.Write("Комфорт: ");
+                string comfort = Console.ReadLine();
+
+                list.Add(new Zakaz(fam, size, comfort));
+            }
+
+            foreach (var z in list) z.Show();
+        }
+    }
+}
---
+++ Zakaz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    internal class Zakaz
+    {
+        public string Fam { get; set; }
+        public int Size { get; set; }
+        public string Comfort { get; set; }
+
+        public Zakaz() { }
+        public Zakaz(string fam) => Fam = fam;
+        public Zakaz(string fam, int size) : this(fam) => Size = size;
+        public Zakaz(string fam, int size, string comfort) : this(fam, size) => Comfort = comfort;
+
+        public void Show()
+        {
+            Console.WriteLine($"Фамилия: {Fam}, Мест: {Size}, Комфорт: {Comfort}");
+        }
+    }
+}
