18749f19a4302f9d89908dc6f965218d7a58e524
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:07:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate double AvgDelegate(int m, int n);
+
+namespace ConsoleApp46
+{
+    internal class Program
+    {
+        static double Average(int m, int n)
+        {
+            int sum = 0;
+            int count = 0;
+            for (int i = m; i <= n; i++)
+            {
+                sum += i;
+                count++;
+            }
+            return (double)sum / count;
+        }
+        static void Main(string[] args)
+        {
+            AvgDelegate del = Average;
+            Console.WriteLine(del(1, 10));
+            Console.WriteLine(del(5, 15));
+            Console.WriteLine(del(10, 20));
+        }
+    }
+}
