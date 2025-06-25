707169f7dd857f09f81872e6485ec24c7f9178a8
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:17:28 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp55
+{
+    internal class Point
+    {
+        public static int[] quarters = new int[4];
+        public static int total = 0;
+        int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            total++;
+
+            if (x > 0 && y > 0) quarters[0]++;
+            else if (x < 0 && y > 0) quarters[1]++;
+            else if (x < 0 && y < 0) quarters[2]++;
+            else if (x > 0 && y < 0) quarters[3]++;
+        }
+
+        public void Print() => Console.WriteLine($"Точка: ({x}, {y})");
+
+        public static void PrintStats()
+        {
+            for (int i = 0; i < 4; i++) Console.WriteLine($"{i + 1} четверть: {quarters[i]}");
+            Console.WriteLine($"Всего точек: {total}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp55
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Point(2, 3);
+            var p2 = new Point(-1, 4);
+            var p3 = new Point(-2, -2);
+            var p4 = new Point(3, -1);
+
+            p1.Print(); p2.Print(); p3.Print(); p4.Print();
+            Point.PrintStats();
+        }
+    }
+}
