8ad5d4879ac87e2a5494a0ba82b4758a56a250f2
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:01:36 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    internal class Person: IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other) => Name.CompareTo(other.Name);
+    }
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person> {
+            new Person { Name = "Олег", Age = 30 },
+            new Person { Name = "Андрей", Age = 25 },
+            new Person { Name = "Мария", Age = 22 },
+            new Person { Name = "Вика", Age = 27 }
+        };
+
+            people.Sort();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Name}, возраст {person.Age}");
+            }
+        }
+    }
+}
