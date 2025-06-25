8dcbb77f026e7ffd2ef817e8fd8e1032f5549921
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:16:36 +0300

    Add files via upload

---
+++ Kupyura.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    internal class Kupyura
+    {
+        public string Name { get; set; }
+        private int nominal;
+        public int Nominal
+        {
+            get => nominal;
+            set => nominal = value > 0 ? value : 1;
+        }
+
+        private int count;
+        public int Count
+        {
+            get => count;
+            set => count = value >= 0 ? value : 0;
+        }
+
+        public Kupyura() { }
+        public Kupyura(string name) => Name = name;
+        public Kupyura(string name, int nom, int cnt)
+        {
+            Name = name;
+            Nominal = nom;
+            Count = cnt;
+        }
+
+        public void ChangeCount(int delta) => Count += delta;
+        public bool CanPay(int price) => (Nominal * Count) >= price;
+
+        public void Print()
+        {
+            Console.WriteLine($"Название: {Name}, Номинал: {Nominal}, Кол-во: {Count}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var k1 = new Kupyura("Рубль", 50, 10);
+            var k2 = new Kupyura("Доллар", 100, 2);
+            var k3 = new Kupyura("Евро", 200, 5);
+            k1.Print(); k2.Print(); k3.Print();
+
+            Console.WriteLine("Можно ли купить за 500: " + k3.CanPay(500));
+        }
+    }
+}
