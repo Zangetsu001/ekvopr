18ed48f6a343cfe242bedf6903e5e2bdc0747621
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 20:18:37 +0300

    Add files via upload

---
+++ DateClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp56
+{
+    internal class DateClass
+    {
+        public DateTime Data { get; set; }
+
+        public DateClass() => Data = DateTime.Now;
+        public DateClass(int year, int month, int day) => Data = new DateTime(year, month, day);
+
+        public DateTime PrevDay() => Data.AddDays(-1);
+        public DateTime NextDay() => Data.AddDays(1);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp56
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Год: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Месяц: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("День: ");
+            int d = int.Parse(Console.ReadLine());
+
+            var date = new DateClass(y, m, d);
+            Console.WriteLine("Текущая: " + date.Data.ToShortDateString());
+            Console.WriteLine("Вчера: " + date.PrevDay().ToShortDateString());
+            Console.WriteLine("Завтра: " + date.NextDay().ToShortDateString());
+        }
+    }
+}
