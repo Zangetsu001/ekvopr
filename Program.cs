eabf055523e5ecc5729d2e377cbaf91e1b00e4ba
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-25 19:47:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TourFirm> firms = new List<TourFirm>
+        {
+            new TourFirm("Солнышко", 2010, "123456", 50000),
+            new TourFirm("Путевка+", 2015, "234567", 70000),
+            new TourFirm("МирТур", 2012, "345678", 30000)
+        };
+
+            Console.WriteLine("Введите название фирмы для поиска:");
+            string name = Console.ReadLine();
+            var found = firms.FirstOrDefault(f => f.Name == name);
+            if (found != null)
+                Console.WriteLine($"ИНН: {found.INN}, Прибыль: {found.Profit}");
+            else
+                Console.WriteLine("Фирма не найдена.");
+
+            double total = firms.Sum(f => f.Profit);
+            Console.WriteLine($"Суммарная прибыль всех фирм: {total}");
+
+            var maxProfitFirm = firms.OrderByDescending(f => f.Profit).First();
+            Console.WriteLine($"Максимальная прибыль у фирмы {maxProfitFirm.Name}: {maxProfitFirm.Profit}");
+
+            firms.RemoveAll(f => f.Profit <= 0);
+            firms.Add(new TourFirm("НоваяФирма", 2024, "999999", 60000));
+
+            Console.WriteLine("Финальный список фирм:");
+            foreach (var f in firms)
+                f.ShowInfo();
+        }
+    }
+}
---
+++ TourFirm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class TourFirm
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string INN { get; set; }
+        public double Profit { get; set; }
+
+        public TourFirm(string name, int year, string inn, double profit)
+        {
+            Name = name;
+            Year = year;
+            INN = inn;
+            Profit = profit;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} | Год: {Year} | ИНН: {INN} | Прибыль: {Profit} руб.");
+        }
+    }
+}
